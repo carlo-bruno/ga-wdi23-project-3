54da98b04f4ca2994bbde06887fbff3a86e59643
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-03-25 09:48:15 -0700

    adding links to elections page

--- client/src/Pages/Elections.jsx
+++ client/src/Pages/Elections.jsx
@@ -3,9 +3,23 @@ import React from 'react';
 const Elections = (props) => {
     return (
         <div className='Elections'>
-            <div className='election-link' href='/'>Elections</div>
-            <div className='registration' href='/'>Registration</div>
-            <div className='locations' href='/'>Locations</div>
+            <div className='election-link'>
+                <h3>Elections</h3>
+                <p><a href='https://kingcounty.gov/depts/elections/results/2019/201902.aspx'>February 2019 special election</a></p>
+                <p><a href='https://kingcounty.gov/depts/elections/results/2019/201904.aspx'>April 2019 special election</a></p>
+                <p><a href='https://kingcounty.gov/depts/elections/results/2019/201908.aspx'>August 2019 primary election</a></p>
+                <p><a href='https://kingcounty.gov/depts/elections/results/2019/201911.aspx'>November 2019 general election</a></p>
+            </div>
+
+            <div className='registration'>
+                <h3>Registration</h3>
+                <p><a href='https://kingcounty.gov/depts/elections/how-to-vote/register-to-vote.aspx'>Register to vote</a></p>
+                <p><a href='https://weiapplets.sos.wa.gov/MyVote/#/login'>Check if I'm registered</a></p>
+            </div>
+
+            <div className='locations'>Locations
+                <p><a href='</p>
+            </div>
             <div className='volunteer' href='/'>Volunteer</div>
             <div className='candidates' href='/'>Candidates</div>
         </div>
