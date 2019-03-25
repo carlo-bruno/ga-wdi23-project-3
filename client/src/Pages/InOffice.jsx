7d1a10a52a31d1d480e9b7ca1b7ed6eb79ede14b
Hannah Erickson <hannaherickson@Hannahs-MacBook-Air.local>
2019-03-24 22:03:13 -0700

    rep links

--- client/src/Pages/InOffice.jsx
+++ client/src/Pages/InOffice.jsx
@@ -3,31 +3,29 @@ import React from 'react';
 const InOffice = (props) => {
     return (
         <div className='RepContainer'> 
-        <header>WA State Representatives</header>
-            <div className='rep-1' href='/'>Rep #1</div>
-            <div className='rep-2' href='/'>Rep #2</div>
-            <div className='rep-3' href='/'>Rep #3</div>
-            <div className='rep-4' href='/'>Rep #4</div>
-            <div className='rep-5' href='/'>Rep #5</div>
-            <div className='rep-6' href='/'>Rep #6</div>
-            <div className='rep-7' href='/'>Rep #7</div>
-            <div className='rep-8' href='/'>Rep #8</div>
-            <div className='rep-9' href='/'>Rep #9</div>
-            <div className='rep-10' href='/'>Rep #10</div>
+        <header>City of Seattle Representatives</header>
+            <div className='rep-1' href='https://www.seattle.gov/mayor'>Mayor Jennt A. Durkan</div>
+            <div className='rep-2' href='https://www.seattle.gov/council/harrell'>Bruce A Harrell - Council President</div>
+            <div className='rep-3' href='https://www.seattle.gov/council/herbold'>Lisa Herbold - Councilmember</div>
+            <div className='rep-4' href='https://www.seattle.gov/council/mosqueda'>Teresa Mosqueda - Councilmember</div>
+            <div className='rep-5' href='https://www.seattle.gov/council/bagshaw'>Sally Bagshaw - Councilmember</div>
+            <div className='rep-6' href='https://www.seattle.gov/council/johnson'>Rob Johnson - Councilmember</div>
+            <div className='rep-7' href='https://www.seattle.gov/council/obrien'>Mike O'Brien - Councilmember</div>
+            <div className='rep-8' href='https://www.seattle.gov/council/gonzalez'>M. Lorena Gonzalez - Councilmember</div>
+            <div className='rep-9' href='https://www.seattle.gov/council/juarez'>Debora Juarez - Councilmember</div>
+            <div className='rep-10' href='https://www.seattle.gov/council/sawants'>Kshama Sawant - Councilmember</div>
         <header>King County Representatives</header>
-        <div className='rep-1' href='/'>Rep #1</div>
-            <div className='rep-2' href='/'></div>
-            <div className='rep-10' href='/'>Dow Constantine - King County Executive</div>
-            <div className='rep-3' href='/'>Rod Dembowski - District 1</div>
-            <div className='rep-4' href='/'>Larry Gossett - District 2</div>
-            <div className='rep-5' href='/'>Kathy Lambert - District 3</div>
-            <div className='rep-6' href='/'>Jeanne Kohl-Welles - District 4</div>
-            <div className='rep-7' href='/'>Dave Upthegrove - District 5</div>
-            <div className='rep-8' href='/'>Claudia Balducci - District 6</div>
-            <div className='rep-9' href='/'>Pete Von Reichbauer - District 7</div>
-            <div className='rep-10' href='/'>Joe McDermott - District 8</div>
-            <div className='rep-10' href='/'>Reagan Dunn - District 9</div>
-
+            <div className='rep-10' href='https://www.kingcounty.gov/elected/executive.aspx'>Dow Constantine - King County Executive</div>
+            <div className='rep-3' href='https://www.kingcounty.gov/council/dembowski.aspx'>Rod Dembowski - District 1</div>
+            <div className='rep-4' href='https://www.kingcounty.gov/council/gossett.aspx'>Larry Gossett - District 2</div>
+            <div className='rep-5' href='https://www.kingcounty.gov/council/lambert.aspx'>Kathy Lambert - District 3</div>
+            <div className='rep-6' href='https://kingcounty.gov/council/kohl-welles.aspx'>Jeanne Kohl-Welles - District 4</div>
+            <div className='rep-7' href='https://www.kingcounty.gov/council/upthegrove.aspx'>Dave Upthegrove - District 5</div>
+            <div className='rep-8' href='https://kingcounty.gov/council/balducci.aspx'>Claudia Balducci - District 6</div>
+            <div className='rep-9' href='https://www.kingcounty.gov/council/vonReichbauer.aspx'>Pete Von Reichbauer - District 7</div>
+            <div className='rep-10' href='https://www.kingcounty.gov/council/mcdermott.aspx'>Joe McDermott - District 8</div>
+            <div className='rep-10' href='https://www.kingcounty.gov/council/dunn.aspx'>Reagan Dunn - District 9</div>
+            <div className='districtMap' href='https://www.kingcounty.gov/council/councilmembers/find_district.aspx'>Find Your District</div>
         </div>
     )
 }
