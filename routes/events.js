7b25e085280e3bd1b8c4da1b21b687cfe43becb3
Garrett Moore <robertgarrettmoore@gmail.com>
2019-03-26 08:55:27 -0700

    basic post route for events

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const Event = require('./models/event')
 
 // Meetup API
 function getMeetUps() {
@@ -87,6 +88,21 @@ router.get('/', (req, res) => {
       })
     )
     .catch((err) => res.json({ err }));
+});
+
+router.post('/',  (req, res) => {
+  let event = new Event({
+    _id: req.body._id,
+      time: req.body.time,
+			date: req.body.date,
+			location: req.body.location,
+			title: req.body.title,
+			description: req.body.description,
+			usernotes: req.body.usernotes
+  });
+  event.save( (err, doc) => {
+    res.json(doc);
+  })
 });
 
 module.exports = router;
