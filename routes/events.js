2258016b0710b95c334e664ed96d2c33e65d4bdf
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-03-26 10:23:25 -0700

    content for elections page

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const Event = require('./models/event')
+const Event = require('../models/event')
 
 // Meetup API
 function getMeetUps() {
