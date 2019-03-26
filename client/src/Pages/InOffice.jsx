324751aec9806c53d2343f2bfb102b82b8b08975
Carlo Bruno <carlofbruno@gmail.com>
2019-03-26 12:42:43 -0700

    post merge cleanup

--- client/src/Pages/InOffice.jsx
+++ client/src/Pages/InOffice.jsx
@@ -1,43 +1,37 @@
 import React from 'react';
-<<<<<<< HEAD
-import { city } from '../data/office';
 
-=======
-import OfficeCard from '../Components/OfficeCard'
-import {city, county} from '../data/office'
-
-
+import OfficeCard from '../Components/OfficeCard';
+import { city, county } from '../data/office';
 
 const InOffice = (props) => {
+  let cityReps = city.map((rep, i) => {
+    return (
+      <OfficeCard
+        key={i}
+        name={rep.name}
+        title={rep.title}
+        img={rep.img}
+      />
+    );
+  });
 
-
-
-  let cityReps = city.map(rep => {
-     return(<div> 
-      <OfficeCard name={rep.name} title={rep.title} img={rep.img} />
-    </div>)
-
-  })
-
-  let countyReps = county.map(rep => {
-    return(<div> 
-      <OfficeCard name={rep.name} title={rep.title} img={rep.img}/>
-   </div>)
-
- })
+  let countyReps = county.map((rep, i) => {
+    return (
+      <OfficeCard
+        key={i}
+        name={rep.name}
+        title={rep.title}
+        img={rep.img}
+      />
+    );
+  });
 
   return (
-    <div>
-    {cityReps}
-    {countyReps}
-      {
-        // OfficeCard
-        // OfficeCard
-        // OfficeCard
-        //
-      }
+    <div className='InOffice'>
+      {cityReps}
+      {countyReps}
     </div>
   );
 };
->>>>>>> 22da874e415c60967ffb6f7071d26462e86426bb
 
+export default InOffice;
